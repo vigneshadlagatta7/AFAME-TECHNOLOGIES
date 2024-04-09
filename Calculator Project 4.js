cdfe8a77eff5867ff063da64f8271c9433cf89d8
vigneshadlagatta7 <114798401+vigneshadlagatta7@users.noreply.github.com>
2024-04-09 23:06:41 +0530

    Add files via upload

---
+++ Calculator Project 4.js
@@ -0,0 +1,16 @@
+function appendValue(value) {
+    document.getElementById('result').value += value;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
